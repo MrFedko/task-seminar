1e338f5859fabaa7f1b3c9c29cb1cbc8c5f8954d
Mr Fedko <miklerst@mail.ru>
2021-11-06 13:15:11 +0300

    вычисление sin^p(a по введенным P и a)

--- 0005/Program.cs
+++ 0005/Program.cs
@@ -6,6 +6,6 @@ Console.WriteLine("Введите число a: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
 double sinAngle = Math.Pow(Math.Sin(a),P); // Math.Sin() - вычисляет синус. 
-// а конструкция Math.Pow(a, b) возводит число "a" в степень "b".
+// а конструкция Math.Pow(a, b) возводит число "a" в степень "b" .
 
 Console.WriteLine($"Синус {a} в степени {P} = {sinAngle}");
