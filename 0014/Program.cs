fbad308930869c8a89e9f32f0c850c9972e8ec3f
Mr Fedko <miklerst@mail.ru>
2021-11-06 12:10:24 +0300

    найти третью цифру числа или сообщить что её нет

---
+++ 0014/Program.cs
@@ -0,0 +1,28 @@
+﻿// найти третью цифру числа или сообщить что её нет
+Console.WriteLine("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 1)
+{
+    Console.WriteLine("Это число не натуральное.");
+    return;
+}
+
+if (num < 100 && num > -100)
+{
+    Console.WriteLine("У этого числа нет третьей цифры.");
+    return;
+}
+
+string ourNum = Convert.ToString(num);
+
+string search3(string text)
+{
+    string result = String.Empty;
+
+    result = result + text[2];
+    Console.WriteLine($"Третьей цифрой этого числа является {result}");
+
+    return result;
+}
+search3(ourNum);
