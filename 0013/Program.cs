cec51aa3e4818fc7a232d3fe4190ee56b6f732d1
Mr Fedko <miklerst@mail.ru>
2021-11-06 12:09:56 +0300

    Выяснить кратно ли число заданному. Если нет, вывести остаток.

---
+++ 0013/Program.cs
@@ -0,0 +1,35 @@
+﻿// Выяснить кратно ли число заданному. Если нет, вывести остаток.
+
+Console.WriteLine("Введите натуральное число от 100 до 1000: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число от 1 до 100: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if ((num < 100) || (num > 1000))
+{
+    Console.WriteLine("Это число не из заданного отрезка.");
+    return;
+}
+
+if ((num2 < 1) || (num2 > 99))
+{
+    Console.WriteLine("Это число не из заданного отрезка.");
+    return;
+}
+
+int remainder(int number, int number1)
+{
+    int result = 0;
+    result = number % number1;
+    return result;
+}
+
+if (num % num2 == 0)
+{ 
+    Console.WriteLine($"Число {num} делится на {num2} без остатка");
+}
+else
+{
+    Console.WriteLine($"Остаток от деления {num} на {num2} = {remainder(num, num2)}");
+}
