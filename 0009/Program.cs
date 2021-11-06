e1c2c18e7ef2aca393c96cdfbcd70a1727d40c4a
Mr Fedko <miklerst@mail.ru>
2021-11-06 12:08:07 +0300

    показать последнюю цифру трехзначного числа

---
+++ 0009/Program.cs
@@ -0,0 +1,20 @@
+﻿// показать последнюю цифру трехзначного числа
+
+Console.WriteLine("Введите натуральное трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num < 100) || (num > 999))
+{
+    Console.WriteLine("это не трехзначное натуральное число.");
+    return;
+}
+string lastNum = Convert.ToString(num);
+
+string LastNum(string number)
+{
+    string result = String.Empty;
+    result = result + number[2];
+    return result;
+}
+
+Console.WriteLine($"Последняя цифра этого числа: {LastNum(lastNum)}");
