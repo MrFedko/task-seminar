f0b1041a0edaf8109a148ef255e7c7cc0ddc8bb1
Mr Fedko <miklerst@mail.ru>
2021-11-06 12:09:30 +0300

    удалить вторую цифру трехзначного числа

---
+++ 0012/Program.cs
@@ -0,0 +1,23 @@
+﻿// удалить вторую цифру трехзначного числа
+
+
+Console.WriteLine("Введите натуральное трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num < 100) || (num > 999))
+{
+    Console.WriteLine("это не трехзначное натуральное число.");
+    return;
+}
+string lastNum = Convert.ToString(num);
+
+string NewNumber(string number)
+{
+    string result = String.Empty;
+    result = result + number[0] + number[2];
+    return result;
+}
+
+int num2 = Convert.ToInt16(NewNumber(lastNum));
+
+Console.WriteLine($"Получилось число {num2}");
