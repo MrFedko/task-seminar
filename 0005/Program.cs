53de0a28f4609bdb01cb78c02397ed87188d3d43
Mr Fedko <miklerst@mail.ru>
2021-11-06 12:06:10 +0300

    Написать программу вычисления значения функции y=f(a) = sin в степени p (количество букв фамилии)

---
+++ 0005/Program.cs
@@ -0,0 +1,12 @@
+﻿//Написать программу вычисления значения функции y=f(a) = sin в степени p (количество букв фамилии)
+// y = sin^P(a)
+
+Console.WriteLine("Введите число P: ");
+int P = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+double sinAngle = Math.Pow(Math.Sin(a),P);
+
+
+Console.WriteLine($"{sinAngle}");
