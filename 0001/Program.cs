e9fffd24bbb5c4cb7e1751b6aac967aeb5be4923
Mr Fedko <miklerst@mail.ru>
2021-11-06 10:56:18 +0300

    по двум заданным числам проверять, является первое квадратом второго.

---
+++ 0001/Program.cs
@@ -0,0 +1,26 @@
+﻿// // по двум заданным числам проверять, является первое квадратом второго.
+
+Console.WriteLine("Введите первое число: ");
+long n1 = Convert.ToInt64(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+long n2 = Convert.ToInt64(Console.ReadLine());
+
+void True()
+{
+    Console.WriteLine($"Число {n1} является квадратом числа {n2}");
+}
+void False()
+{
+    Console.WriteLine($"Число {n1} не вляется квадратом числа {n2}");
+}
+
+if (n1 <= 0 || n2 <= 0) Console.WriteLine("Введите натуральные числа.");
+
+if (n2 * n2 == n1) 
+{
+    True();
+}
+else 
+{
+    False();
+}
