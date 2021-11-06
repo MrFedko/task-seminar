163b5b2b8c47c57e3a75c1c7ee4dc4182e8c444d
Mr Fedko <miklerst@mail.ru>
2021-11-06 12:07:34 +0300

    вывести четные числа от 1 до N

---
+++ 0008/Program.cs
@@ -0,0 +1,22 @@
+﻿// вывести четные числа от 1 до N
+
+Console.WriteLine("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 0)
+{
+    Console.WriteLine("это не натуральное число.");
+    return;
+}
+Console.WriteLine();
+
+void PrintEventNum(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine($"{i}");
+        }
+    }
+}
+PrintEventNum(num);
