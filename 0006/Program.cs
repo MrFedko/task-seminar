cee923c7040a5e7c3bba8bf74f51efb3fbc8c403
Mr Fedko <miklerst@mail.ru>
2021-11-06 13:21:17 +0300

    переписал через метод bool

--- 0006/Program.cs
+++ 0006/Program.cs
@@ -3,12 +3,26 @@
 Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32 (Console.ReadLine());
 
-string initialNumber(int number)
+bool even(int number)
 {
-    string answer = String.Empty;
-    if(number % 2 == 0) answer = answer + "Число является чётным";
-    else answer = answer + "Число является нечётным";
-    return answer;
+    return number % 2 ==0;
 }
 
-Console.WriteLine($"{initialNumber(num)}");
+if(even(num) == true)
+{
+    Console.WriteLine($"Число {num} является четным.");
+}
+else 
+{
+    Console.WriteLine($"Число {num} является нечетным.");   
+}
+
+// string initialNumber(int number)
+// {
+//     string answer = String.Empty;
+//     if(number % 2 == 0) answer = answer + "Число является чётным";
+//     else answer = answer + "Число является нечётным";
+//     return answer;
+// }
+
+// Console.WriteLine($"{initialNumber(num)}");
