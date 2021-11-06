9ab7ad3e38591997f2d8530f7c477e8400294b6c
Mr Fedko <miklerst@mail.ru>
2021-11-06 12:06:36 +0300

    выяснить является ли число четным

---
+++ 0006/Program.cs
@@ -0,0 +1,14 @@
+﻿// выяснить является ли число четным
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+string initialNumber(int number)
+{
+    string answer = String.Empty;
+    if(number % 2 == 0) answer = answer + "Число является чётным";
+    else answer = answer + "Число является нечётным";
+    return answer;
+}
+
+Console.WriteLine($"{initialNumber(num)}");
