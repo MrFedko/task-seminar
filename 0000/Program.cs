ff8cfb46d41ad710509ae10709ddda1659c0332d
Mr Fedko <miklerst@mail.ru>
2021-11-06 10:41:47 +0300

    показать квадрат введенного числа

--- 0000/Program.cs
+++ 0000/Program.cs
@@ -1,8 +1,30 @@
 ﻿Console.WriteLine("Введите число, квадрат которого необходимо найти: ");
-long n = Convert.ToInt64(Console.ReadLine());
-long SquareN(long number)
+int number = Convert.ToInt32(Console.ReadLine());
+
+int qwerty = 89;
+int SquareNumber(int num)
 {
-    long square = number * number;
-    return square;
+    int n = num * num;
+    return n;
 }
-Console.WriteLine($"Квадрат {n} = {SquareN(n)}");
+
+Console.WriteLine($"Квадрат {number} = {SquareNumber(number)}");
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("Введите число, квадрат которого необходимо найти: ");
+// long n = Convert.ToInt64(Console.ReadLine());
+// long SquareN(long number)
+// {
+//     long square = number * number;
+//     return square;
+// }
+// Console.WriteLine($"Квадрат {n} = {SquareN(n)}");
