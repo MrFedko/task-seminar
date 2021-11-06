15133bb9d6bb03f0391e041fd31e19e567588bca
Mr Fedko <miklerst@mail.ru>
2021-11-06 10:18:37 +0300

    rename

---
+++ 0000/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число, квадрат которого необходимо найти: ");
+long n = Convert.ToInt64(Console.ReadLine());
+long SquareN(long number)
+{
+    long square = number * number;
+    return square;
+}
+Console.WriteLine($"Квадрат {n} = {SquareN(n)}");
