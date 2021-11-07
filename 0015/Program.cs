7744121cfa5cdf2f0cbdc9abd796ed98d63bf2c1
Mr Fedko <miklerst@mail.ru>
2021-11-07 10:37:32 +0300

    Дано число. Проверить кратно ли оно 7 и 23

---
+++ 0015/Program.cs
@@ -0,0 +1,32 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+bool number (int n)
+{
+    return (n % 7 == 0) && (n % 23 == 0);
+}
+
+if (number(num) == true)
+{
+    Console.WriteLine($"Число {num} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {num} не кратно 7 и 23");
+}
+
+
+
+
+
+// if ((num % 7 == 0) && (num % 23 == 0))
+// {
+//     Console.WriteLine($"Число {num} кратно 7 и 23");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {num} не кратно 7 и 23");
+// }
