cb6ab6e7a87a5e1ab0ce297c87fb3499c6292ded
Mr Fedko <miklerst@mail.ru>
2021-11-07 11:18:11 +0300

    Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y

---
+++ 0018/Program.cs
@@ -0,0 +1,23 @@
+﻿//Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+// - (x+y) = -x * -y
+
+
+bool TrueFalse (int x, int y)
+{
+    int left = -1 * (x + y);
+    int right = -1 *x * (-1 * y);
+    return(left == right);
+}
+
+for (int i = 0; i <= 1; i++)
+    for (int j = 0; j <=1; j++)
+    {
+        if (TrueFalse(i, j))
+        {
+            Console.WriteLine($"При x = {i}  и y = {j} выражение ¬(X ⋁ Y) = ¬X ⋀ ¬Y истинно");
+        }
+        else
+        {
+            Console.WriteLine($"При x = {i}  и y = {j} выражение ¬(X ⋁ Y) = ¬X ⋀ ¬Y ложно");
+        }
+    }
