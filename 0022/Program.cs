61218e207f97c9b22f558ecebfea29351d658dcf
Mr Fedko <miklerst@mail.ru>
2021-11-07 14:31:57 +0300

    Найти расстояние между точками в пространстве 2D

---
+++ 0022/Program.cs
@@ -0,0 +1,20 @@
+﻿//Найти расстояние между точками в пространстве 2D
+
+Console.WriteLine("Введите координату X для точки A: ");
+int AX = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y для точки A: ");
+int AY = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Введите координату X для точки B: ");
+int BX = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y для точки B: ");
+int BY = Convert.ToInt16(Console.ReadLine());
+
+int lengthA = AX - BX;
+int lengthB = AY - BY;
+
+double distanceAB = Math.Sqrt((Math.Pow(lengthA, 2) + Math.Pow(lengthB, 2)));
+
+Console.WriteLine($"Расстояние между точками A и B равно {distanceAB}");
