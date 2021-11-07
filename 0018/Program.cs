8e32679028badcc4baab007464d1635d535a6aec
MrFedko <miklerst@mail.ru>
2021-11-07 17:07:47 +0300

    ¬(X ⋁ Y) = ¬X ⋀ ¬Y

--- 0018/Program.cs
+++ 0018/Program.cs
@@ -2,7 +2,7 @@
 // - (x+y) = -x * -y
 
 
-bool TrueFalse (int x, int y)
+bool TrueFalse(int x, int y)
 {
     bool FlagX = (x != 0), FlagY = (y != 0);
     // if (x == 0) FlagX = false;
@@ -11,22 +11,21 @@ bool TrueFalse (int x, int y)
     // if (y == 0) FlagY = false;
     // else FlagY = true;
 
-    bool left = !(FlagX || FlagY);
-    bool right = !(FlagX) && !(FlagY);
-    // int left = -1 * (x + y);
-    // int right = -1 *x * (-1 * y);
-    return(left == right);
+    bool left = !(FlagX || FlagY);       // int left = -1 * (x + y);
+    bool right = !(FlagX) && !(FlagY);   // int right = -1 *x * (-1 * y);
+
+    return (left == right);
 }
 
 for (int i = 0; i <= 1; i++)
-    for (int j = 0; j <=1; j++)
+    for (int j = 0; j <= 1; j++)
     {
         if (TrueFalse(i, j))
         {
-            Console.WriteLine($"При x = {i}  и y = {j} выражение ¬(X ⋁ Y) = ¬X ⋀ ¬Y истинно");
+            Console.WriteLine($"При x = {i} и y = {j} выражение ¬(X ⋁ Y) = ¬X ⋀ ¬Y истинно");
         }
         else
         {
-            Console.WriteLine($"При x = {i}  и y = {j} выражение ¬(X ⋁ Y) = ¬X ⋀ ¬Y ложно");
+            Console.WriteLine($"При x = {i} и y = {j} выражение ¬(X ⋁ Y) = ¬X ⋀ ¬Y ложно");
         }
     }
