c39669e222daf88576a43d2832a04485857a5003
MrFedko <miklerst@mail.ru>
2021-11-09 10:59:23 +0300

    показать таблицу квадратов чисел от 1 до N

---
+++ 0023/Program.cs
@@ -0,0 +1,14 @@
+﻿// показать таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите натуральное число: ");
+int N = Convert.ToInt16(Console.ReadLine());
+
+if (N < 1)
+{
+    Console.WriteLine($"Число {N} не является натуральным.");
+    return;
+}
+for ( int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i}^2 = {Math.Pow(i, 2)}");
+}
