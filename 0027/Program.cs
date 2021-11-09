7871489f4b01cf2fbcbb6b3f9ea196f82349d001
MrFedko <miklerst@mail.ru>
2021-11-09 18:56:03 +0300

    определить количество цифр в числе

--- 0027/Program.cs
+++ 0027/Program.cs
@@ -10,10 +10,7 @@ int howLong(double n)
     int count;
     for (count = 1; (temp /= 10) >= 1; count++);
     int count1;
-    for (count1 = 0; n % 1 != 0; count1++)
-    {
-        n *= 10;
-    }
+    for (count1 = 0; n % 1 != 0; count1++) n *= 10;
     int result = count + count1;
     return result;
 }
