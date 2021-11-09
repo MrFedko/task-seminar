a9ada71458d813cd5bc6356b49d403ef7145d151
MrFedko <miklerst@mail.ru>
2021-11-09 17:32:34 +0300

    подсчитать сумму цифр в числе

---
+++ 0028/Program.cs
@@ -0,0 +1,52 @@
+﻿// подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+int howLong(double n)               //необходимо понять насколько длинное число
+{
+    n = Math.Abs(n);
+    int count;
+    for (count = 1; (n /= 10) >= 1; count++) ;
+    return count;
+}
+
+int FractionalDigitCount(double n)          //находим число знаков после запятой
+{
+    n = Math.Abs(n);
+    int count = 0;
+    while (n % 1 != 0)
+    {
+        count++;
+        n *= 10;
+    }
+    return count;
+}
+int length = howLong(number);                    // получаем длинну числа
+int fractional = FractionalDigitCount(number);   //получаем количество знаков после запятой   
+
+Console.WriteLine($" длинна числа = {length}, количество знаков после запятой = {fractional}");
+int summ(double n, int l, int f)                       //вводим число, его длинну, количество знаков после запятой
+{
+    int result = 0;
+    double divider = 1;
+    double temp = 0;
+    if (f > 0)
+    {
+        for (int i = 0; i < f; i++)
+        {
+            double temp1 = 0;
+            temp1 = divider / 10;
+            divider = temp1;
+        }
+    }
+    for (int j = 0; j < (f + l); j++)
+    {
+        temp = (n / divider) % 10;
+        result = Convert.ToInt16(result + temp);
+        divider = divider * 10;
+    }
+    return Math.Abs(result);
+}
+
+Console.WriteLine($"Сумма цифр числа {number} = {summ(number, length, fractional)}");
