22497758cd69fce97742db510d3e56aba0a476fb
MrFedko <miklerst@mail.ru>
2021-11-09 10:50:02 +0300

    исправил несоответствие четвертей

--- 0020/Program.cs
+++ 0020/Program.cs
@@ -13,6 +13,6 @@ int max = 2147483647;
 string[] Qname = { "0", "2 147 483 647", "-2 147 483 647" };
 
 if (quater == 1) Console.WriteLine($"Четверть №{quater} обладает диапозоном X(0, {max}); Y(0, {max})");
-if (quater == 2) Console.WriteLine($"Четверть №{quater} обладает диапозоном X(0, {max}); Y(0, {min})");
+if (quater == 2) Console.WriteLine($"Четверть №{quater} обладает диапозоном X(0, {min}); Y(0, {max})");
 if (quater == 3) Console.WriteLine($"Четверть №{quater} обладает диапозоном X(0, {min}); Y(0, {min})");
-if (quater == 4) Console.WriteLine($"Четверть №{quater} обладает диапозоном X(0, {min}); Y(0, {max})");
+if (quater == 4) Console.WriteLine($"Четверть №{quater} обладает диапозоном X(0, {max}); Y(0, {min})");
