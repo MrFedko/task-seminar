af0984fb7bbad20a6d17d7c3ecaf3f2693941565
MrFedko <miklerst@mail.ru>
2021-11-12 10:10:06 +0300

    Написать программу вычисления произведения чисел от 1 до N

---
+++ 0029/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine($"Число {number} не является натуральным.");
+    return;
+}
+
+int summ = 1;
+for (int i = 1; i <= number; i++)
+{
+    summ = summ * i;
+}
+Console.WriteLine($"Произведение чисел от 1 до {number} = {summ}");
