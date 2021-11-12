93ef06487f327474160161d1204a9a1e7035c7ca
MrFedko <miklerst@mail.ru>
2021-11-12 10:39:22 +0300

    Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран

---
+++ 0032/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+int length = 8;
+int [] array = new int[length];
+
+for (int i = 0; i < length; i++) array[i] = new Random().Next(0,2);
+for (int j = 0; j < length; j++) Console.WriteLine($"A{j+1} = {array[j]}");
