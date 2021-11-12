e72adc228a95b9428b7df59146486de95242791e
MrFedko <miklerst@mail.ru>
2021-11-12 10:37:34 +0300

    Задать массив из 8 элементов и вывести их на экран

---
+++ 0031/Program.cs
@@ -0,0 +1,6 @@
+﻿//Задать массив из 8 элементов и вывести их на экран
+int length = 8;
+int [] array = new int[length];
+
+for (int i = 0; i < length; i++) array[i] = new Random().Next();
+for (int j = 0; j < length; j++) Console.WriteLine($"A{j+1} = {array[j]}");
