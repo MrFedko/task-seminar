ccba9893e39cb9f4c8e5e9be17db67fa7f992737
MrFedko <miklerst@mail.ru>
2021-11-12 10:14:34 +0300

    Показать кубы чисел, заканчивающихся на четную цифру

---
+++ 0030/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine($"Число {number} не является натуральным.");
+    return;
+}
+
+for ( int i = 1; i <= number; i++)
+{
+    if(i % 2 == 0) Console.WriteLine($"{i}^3 = {Math.Pow(i,3)}");
+}
