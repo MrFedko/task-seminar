3b4cc0561d8f90e9ca39e3ff361b8c7f01c7ecf2
MrFedko <miklerst@mail.ru>
2021-11-13 12:37:29 +0300

    ускорение через математику

--- 0025/Program.cs
+++ 0025/Program.cs
@@ -2,14 +2,14 @@
 
 // Найти сумму чисело от 1 до А
 
-// Console.WriteLine("Введите натуральное число: ");
-// int N = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите натуральное число: ");
+double N = Convert.ToDouble(Console.ReadLine());
 
-// if (N < 1)
-// {
-//     Console.WriteLine($"Число {N} не является натуральным.");
-//     return;
-// }
+if (N < 1)
+{
+    Console.WriteLine($"Число {N} не является натуральным.");
+    return;
+}
 
 double summ(double number)
 {
@@ -23,8 +23,8 @@ double summ2(double number)
     return (number*(number+1))/2;
 }
 DateTime dt = DateTime.Now;
-Console.WriteLine($"Сумма чисел от 1 до 1000000000  = {summ2(1000000000)}");
+Console.WriteLine($"Сумма чисел от 1 до {N}  = {summ(N)}");
 Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
 dt = DateTime.Now;
-Console.WriteLine($"Сумма чисел от 1 до 1000000000  = {summ(1000000000)}");
+Console.WriteLine($"Сумма чисел от 1 до {N}  = {summ2(N)}");
 Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
