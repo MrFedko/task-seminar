3996d078dcb795535e880e7d59d47f0dad1d4a67
MrFedko <miklerst@mail.ru>
2021-11-13 11:16:27 +0300

    Выяснить является ли вещественное m корнем целой степени из 5,

---
+++ 0056.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// // Выяснить является ли вещественное m
+
+// корнем целой степени из 5,
+//         m    5^4
+// 5, 25, 125, 625
+// 1   2   3    4
+
+
+Console.WriteLine("Введите вещественное число M: ");
+double m = Convert.ToDouble(Console.ReadLine());
+
+// Math.Sqrt(m);
+// Math.Pow(m,i)
+for(int i = 1; i < 100; i++)
+{
+    if (Math.Round(Math.Pow(m, i),2) == 5) Console.WriteLine($"Число M {m} является корнем целой степени {i} числа 5");
+}
