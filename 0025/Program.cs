a91a2f4d5899a09705e1c62c95167d4cf0692b7c
MrFedko <miklerst@mail.ru>
2021-11-13 11:58:18 +0300

    супер решениеgit add 0025git add 0025! ускорение работы

--- 0025/Program.cs
+++ 0025/Program.cs
@@ -1,19 +1,30 @@
-﻿// Найти сумму чисело от 1 до А
+﻿
 
-Console.WriteLine("Введите натуральное число: ");
-int N = Convert.ToInt16(Console.ReadLine());
+// Найти сумму чисело от 1 до А
 
-if (N < 1)
+// Console.WriteLine("Введите натуральное число: ");
+// int N = Convert.ToInt16(Console.ReadLine());
+
+// if (N < 1)
+// {
+//     Console.WriteLine($"Число {N} не является натуральным.");
+//     return;
+// }
+
+double summ(double number)
 {
-    Console.WriteLine($"Число {N} не является натуральным.");
-    return;
-}
-
-int summ(int number)
-{
-    int result = 0;
+    double result = 0;
     for (int i = 1; i <= number; i++) result = i + result;
     return result;
 }
 
-Console.WriteLine($"Сумма чисел от 1 до {N} = {summ(N)}");
+double summ2(double number)
+{
+    return (number*(number+1))/2;
+}
+DateTime dt = DateTime.Now;
+Console.WriteLine($"Сумма чисел от 1 до 1000000000  = {summ2(1000000000)}");
+Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+dt = DateTime.Now;
+Console.WriteLine($"Сумма чисел от 1 до 1000000000  = {summ(1000000000)}");
+Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
