9cc7d83f901743876c3e3117f63c75c58b9167a9
MrFedko <miklerst@mail.ru>
2021-11-17 10:33:10 +0300

    Выяснить являются ли три числа сторонами треугольника

--- 0041/Program.cs
+++ 0041/Program.cs
@@ -7,7 +7,11 @@ int B = Convert.ToInt16(Console.ReadLine());
 Console.WriteLine("Введите натуральное число C: ");
 int C = Convert.ToInt16(Console.ReadLine());
 
-if ((A < (B + C)) && (B < (A + C)) && (C < (A + B))) Console.WriteLine($"Числа A = {A}, B = {B}, C = {C} являются сторонами треугольника");
+bool triangle ( int q, int w, int e)
+{
+    return (q < (w + e)) && (w < (q + e)) && (e < (q + w));
+}
+if (triangle(A, B, C) == true) Console.WriteLine($"Числа A = {A}, B = {B}, C = {C} являются сторонами треугольника");
 else 
 { 
     Console.WriteLine($"Числа A = {A}, B = {B}, C = {C} НЕ являются сторонами треугольника");
