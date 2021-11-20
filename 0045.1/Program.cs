572c20d28a2fa75952aac5f9a9bafdb9d5497607
MrFedko <miklerst@mail.ru>
2021-11-20 15:57:47 +0300

    fibonacci

---
+++ 0045.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// fibonacci
+
+double fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine(fibonacci(i));
+}
