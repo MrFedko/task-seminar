f20abb12bfae956bb1833c1b2113cbf9ea261ff1
MrFedko <miklerst@mail.ru>
2021-11-20 11:02:22 +0300

    введение одной строкой

--- 0042/Program.cs
+++ 0042/Program.cs
@@ -1,17 +1,36 @@
 ﻿// Определить сколько чисел больше 0 введено с клавиатуры
-int length = 6;
-int[] array = new int[length];
+// int length = 6;
+// int[] array = new int[length];
 
-int counter(int[] a)
+// int counter(int[] a)
+// {
+//     int c = 0;
+//     for (int i = 0; i < a.Length; i++)
+//     {
+//         Console.WriteLine($"Введите элемент массива A{i}: ");
+//         a[i] = Convert.ToInt32(Console.ReadLine());
+//         if (a[i] > 0) c++;
+//     }
+//     return c;
+// }
+
+// Console.WriteLine($"В массиве {counter(array)} чисел больше 0");
+
+
+
+string numbers = Console.ReadLine();
+string[] num = numbers.Split(' ');
+
+int counter(string[] i)
 {
-    int c = 0;
-    for (int i = 0; i < a.Length; i++)
+    int count = 0;
+    for (int j = 0; j < i.Length; j++)
     {
-        Console.WriteLine($"Введите элемент массива A{i}: ");
-        a[i] = Convert.ToInt32(Console.ReadLine());
-        if (a[i] > 0) c++;
+        int temp = Convert.ToInt32(i[j]);
+        if (temp > 0) count++;
     }
-    return c;
+    return count;
 }
 
-Console.WriteLine($"В массиве {counter(array)} чисел больше 0");
+Console.WriteLine($"ответ: {counter(num)}");
+
