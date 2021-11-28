54c6ee808aad52a0496624fe67a1a11f93dc5b84
MrFedko <miklerst@mail.ru>
2021-11-28 12:07:22 +0300

    Найти сумму цифр числа

---
+++ 0070/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму цифр числа
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt16(Console.ReadLine());
+int tempor = 0;
+
+int Summ(int num, int div, int temp)
+{
+    if (num / div == 0) return temp;
+    temp = temp + (num / div) % 10;
+    return Summ(num, div * 10, temp);
+}
+
+Console.WriteLine(Summ(number, 1, tempor));
