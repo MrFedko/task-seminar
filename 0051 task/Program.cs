b102ff39a8bd4f5593180ae00a52536d46f1f074
MrFedko <miklerst@mail.ru>
2021-11-28 11:19:50 +0300

    Задать двумерный массив следующим правилом: Aₘₙ = m+n

---
+++ 0051 task/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+Console.WriteLine("Введите число столбцов: ");
+int num1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите число строк: ");
+int num2 = Convert.ToInt16(Console.ReadLine());
+
+int[,] NewArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    return array;
+}
+
+int[,] WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    return array;
+}
+
+NewArray(num1, num2);
