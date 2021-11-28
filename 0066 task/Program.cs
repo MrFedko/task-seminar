0975c2fb6b5acb2cf20d4106d91a6d2c9b45ffa5
MrFedko <miklerst@mail.ru>
2021-11-28 11:41:27 +0300

    Показать натуральные числа от 1 до N, N задано

---
+++ 0066 task/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt16(Console.ReadLine());
+int number1 = 1;
+
+int ShowNumber(int n1, int n)
+{
+    Console.WriteLine($"{n1}");
+    if (n1 == n) return n;
+    return ShowNumber(n1 + 1, n);
+}
+
+ShowNumber(1, number);
