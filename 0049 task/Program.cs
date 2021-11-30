e200fa30b9e273c7a673077f3e58e595fac14604
MrFedko <miklerst@mail.ru>
2021-11-30 11:42:08 +0300

    Показать двумерный массив размером m×n заполненный вещественными числами

---
+++ 0049 task/Program.cs
@@ -0,0 +1,38 @@
+﻿//  Показать двумерный массив размером m×n заполненный вещественными числами
+
+Console.WriteLine("Введите число строк: ");
+int num1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите число столбцов: ");
+int num2 = Convert.ToInt16(Console.ReadLine());
+
+double[,] NewArray(int a, int b)
+{
+    double[,] array = new double[a, b];
+    return array;
+}
+
+double [,] FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Math.Round((new Random().Next() + new Random().NextDouble()),4);
+        }
+        return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double [,] MyArray = NewArray(num1, num2);
+FillArray(MyArray);
+PrintArray(MyArray);
