55f17d838f39a1ce71e0f313e169a3447ff96883
MrFedko <miklerst@mail.ru>
2021-11-30 10:11:24 +0300

    высокоточный таймер

---
+++ 0051.1 fast time/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+const int collumn = 4000;
+int[,] array = new int[collumn, collumn];
+
+int sum = 0;
+
+Stopwatch sv = new (); //высокоточный таймер
+sv.Start();
+
+for (int i = 0; i < collumn; i++)
+    for (int j = 0; j < collumn; j++)
+    {
+        sum += array[i, j];
+    }
+sv.Stop();
+
+Console.WriteLine($"{sv.ElapsedMilliseconds}");
