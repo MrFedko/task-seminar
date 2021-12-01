b2aecb4cea615dd7a5184218eddca32a2ba7b0df
MrFedko <miklerst@mail.ru>
2021-12-01 11:24:57 +0300

    Составить частотный словарь элементов двумерного массива

---
+++ 0060 task/Program.cs
@@ -0,0 +1,41 @@
+﻿// Составить частотный словарь элементов двумерного массива
+
+string little = "Так как я никогда в жизни не рисовал баранов"
+                + " я повторил для него одну из двух старых картинок"
+                + " которые я только и умею рисовать удава снаружи";
+
+var littleArray = little.Split(" ").ToArray();
+
+void PrintArrayNew(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+}
+
+int[] Dictionary(string[] array)
+{
+    int[] array1 = new int[array.Length];
+    int temp = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length; j++)
+        {
+            if (array[i] == array[j]) temp++;
+            array1[i] = temp;
+        }
+        temp = 0;
+    }
+    return array1;
+}
+
+PrintArrayNew(littleArray);
+PrintArray(Dictionary(littleArray));
