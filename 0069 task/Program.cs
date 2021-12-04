b8234dba57a6b2f84562f05b05cccb897d471560
MrFedko <miklerst@mail.ru>
2021-12-04 10:45:16 +0300

    Найти сумму элементов от M до N, N и M заданы

---
+++ 0069 task/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Введите число M: ");
+int number1 = Convert.ToInt16(Console.ReadLine());
+
+int ShowNumber(int m, int n, int sum)
+{
+    sum += m;
+    if (m == n) return sum;
+    if (m > n) return ShowNumber(m - 1, n, sum);
+    else return ShowNumber(m + 1, n, sum);
+}
+
+int summOfAll = 0;
+Console.WriteLine(ShowNumber(number1, number, summOfAll));
