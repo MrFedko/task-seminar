aaca3146e86cb1c4f2e7d8cbff55913b9677608e
MrFedko <miklerst@mail.ru>
2021-12-04 17:57:09 +0300

    Написать программу возведения числа А в целую стень B

---
+++ 0072 task/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите целую степень числа А: ");
+int degree = Convert.ToInt16(Console.ReadLine());
+
+int NumberDegree(int num, int deg)
+{
+    if (deg == 0) return 1;
+    else if (deg % 2 == 0) return NumberDegree(num * num, deg / 2);
+    else return NumberDegree(num, deg - 1) * num;
+}
+
+Console.WriteLine(NumberDegree(numberA, degree));
