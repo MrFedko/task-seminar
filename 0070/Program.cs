466d66bebd97d031c0c6651dfc65e9eed3a9a33f
MrFedko <miklerst@mail.ru>
2021-12-04 18:30:50 +0300

    Найти сумму цифр числа

--- 0070/Program.cs
+++ 0070/Program.cs
@@ -2,10 +2,10 @@
 Console.WriteLine("Введите число N: ");
 int number = Convert.ToInt16(Console.ReadLine());
 
-int Summ(int num, int div)
+int Summ(int num, int div = 1)
 {
     if (num / div == 0) return 0;
     else return ((num / div) % 10) + Summ(num, div * 10);
 }
 
-Console.WriteLine(Summ(number, 1));
+Console.WriteLine(Summ(number));
