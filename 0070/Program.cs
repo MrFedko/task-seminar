813aca9bdabb70f4b6cce30f7bd7d37189600b5c
MrFedko <miklerst@mail.ru>
2021-12-04 10:54:07 +0300

    Найти сумму цифр числа

--- 0070/Program.cs
+++ 0070/Program.cs
@@ -6,7 +6,7 @@ int tempor = 0;
 int Summ(int num, int div, int temp)
 {
     if (num / div == 0) return temp;
-    temp = temp + (num / div) % 10;
+    temp += (num / div) % 10;
     return Summ(num, div * 10, temp);
 }
 
