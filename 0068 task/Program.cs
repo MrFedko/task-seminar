9d77307437c83370a08dfc343ebb78b441a71f5d
MrFedko <miklerst@mail.ru>
2021-12-04 10:33:14 +0300

    Показать натуральные числа от M до N, N и M заданы

---
+++ 0068 task/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+Console.WriteLine("Введите число N: ");
+int number = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Введите число M: ");
+int number1 = Convert.ToInt16(Console.ReadLine());
+
+int ShowNumber(int n1, int n)
+{
+    Console.WriteLine($"{n1}");
+    if (n1 == n) return n;
+    if (n1 > n) return ShowNumber(n1 - 1, n);
+    else return ShowNumber(n1 + 1, n);
+}
+
+ShowNumber(number1, number);
