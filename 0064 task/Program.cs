da3004fe61629ca2e0b404e2fe9c3be83abc0dfd
MrFedko <miklerst@mail.ru>
2021-12-04 10:06:05 +0300

    Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника

---
+++ 0064 task/Program.cs
@@ -0,0 +1,30 @@
+﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+
+Console.WriteLine("Введите нужное количество строк треугольника Паскаля:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Factorial(int n)
+        {
+            int x = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                x *= i;
+            }
+            return x;
+        }
+
+for (int i = 0; i < n; i++)
+            {
+                for (int c = 0; c <= (n - i); c++)
+                {
+                    Console.Write(" "); 
+                }
+                for (int c = 0; c <= i; c++)
+                {
+                    Console.Write(" "); 
+                    Console.Write(Factorial(i) / (Factorial(c) * Factorial(i - c))); 
+                }
+                Console.WriteLine();
+                Console.WriteLine(); 
+            }
+            Console.ReadLine();
