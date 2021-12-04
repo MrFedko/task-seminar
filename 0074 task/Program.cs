c6e8dec98bd2480399be2df375a6b81a7b0117f0
MrFedko <miklerst@mail.ru>
2021-12-04 18:55:10 +0300

    Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита

---
+++ 0074 task/Program.cs
@@ -0,0 +1,26 @@
+﻿// В некотором машинном алфавите имеются четыре буквы «х», «й», «о» и «п». 
+// Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита
+
+int Request(string message)
+{
+    Console.WriteLine(message);
+    string line = Console.ReadLine();
+    return Convert.ToInt32(line);
+}
+
+int count = Request("Введите количество букв: ");
+
+void NewWords(string our, char[] words, int length = 0)
+{
+    if (length == words.Length)
+    {
+        Console.WriteLine($"{new String(words)}"); return;
+    }
+    for (int i = 0; i < our.Length; i++)
+    {
+        words[length] = our[i];
+        NewWords(our, words, length + 1);
+    }
+}
+
+NewWords("хйоп", new char[count]);
