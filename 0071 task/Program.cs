2270229b266ad7fb89caa42301742f991f4ada64
MrFedko <miklerst@mail.ru>
2021-12-04 11:11:03 +0300

    Написать программу вычисления функции Аккермана

---
+++ 0071 task/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления функции Аккермана
+
+// A(m,n) =    if m = 0             n+1
+//             if m > 0  n = 0      a(m-1,1)
+//             if m> 0, n > 0       A(m-1, A(m, n -1)) 
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return (n + 1);
+    if (m != 0 && n == 0) return Akkerman(m - 1, 1);
+    if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return Akkerman(m,n);
+}
+
+Console.WriteLine(Akkerman(2, 4));
