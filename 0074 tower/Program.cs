9d99d54a8873239abd637ac03282ee0982fe6fcf
MrFedko <miklerst@mail.ru>
2021-12-07 16:03:23 +0300

    ханойская башня

---
+++ 0074 tower/Program.cs
@@ -0,0 +1,10 @@
+﻿// ханойская башня
+
+void Tower(string with = "1", string on = "3", string some = "2", int count = 5)
+{
+    if (count > 1) Tower(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Tower(some, on, with, count - 1);
+}
+
+Tower();
